8d7cdbbcec9fe9e14e65321c9017231d3642ef30
Oji Ofong <ojiofong@gmail.com>
2019-03-23 23:37:58 -0400

    Add a basic HomeScreen populated with mock user data

---
+++ lib/home/HomeListItem.dart
@@ -0,0 +1,54 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:my_braider/home/model/HomeItem.dart';
+import 'package:smooth_star_rating/smooth_star_rating.dart';
+
+class HomeListItem extends StatelessWidget {
+  HomeListItem(this.user);
+
+  final User user;
+
+  void _showSnackBar(BuildContext context, String msg) {
+    Scaffold.of(context).showSnackBar(SnackBar(content: Text(msg)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+
+    return Column(
+      children: <Widget>[
+        GestureDetector(
+          onTap: () {
+            _showSnackBar(context, "onTap ${user.displayName}");
+          },
+          onLongPress: () {
+            _showSnackBar(context, "onLongPress ${user.displayName}");
+          },
+          child: Container(
+            height: 200,
+            width: screenWidth,
+            child: CachedNetworkImage(
+              imageUrl: user.coverPhotoUrl,
+              fit: BoxFit.cover,
+//            placeholder: (context, url) => CircularProgressIndicator(),
+              errorWidget: (context, url, error) => Icon(Icons.error),
+              fadeOutDuration: Duration(seconds: 1),
+              fadeInDuration: Duration(seconds: 2),
+            ),
+          ),
+        ),
+        Text(
+          user.displayName,
+          textScaleFactor: 1.5,
+        ),
+        Text(user.address),
+        Text("User ratings ${user.ratings}"),
+        SmoothStarRating(
+          starCount: 5,
+          rating: 4.4,
+        )
+      ],
+    );
+  }
+}
